ded24a34371b791543faf45bbcf5d7b2d6b2acf3
bluMATRIKZ <blumatrikz@gmail.com>
2025-06-02 11:44:45 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    char name[50];
-
-    printf("Hello! Welcome to the C test program :P.\n");
-
-    printf("Enter your name: ");
-    scanf("%49s", name);  // Limits input size to prevent buffer overflow
-
-    printf("Nice to meet you, %s!\n", name);
+    printf("Nice to meet you\n);
     
     return 0;
 }
